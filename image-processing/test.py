b2d3e8cf07e3a72e101cca64158c9476a8a14715
Mohan <moh4n@yahoo.com>
2022-06-22 20:23:27 +0530

    made changes to image size

--- image-processing/test.py
+++ image-processing/test.py
@@ -1,5 +1,7 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 
 img = Image.open("image1.png")
 filtered_img = img.convert('L')
-filtered_img.save("grey.jpeg", 'jpeg')
+crooked = filtered_img.rotate(180)
+crooked.save("grey.jpeg", 'jpeg')
+crooked.show()
